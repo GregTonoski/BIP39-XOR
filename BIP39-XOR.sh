4e8b8ac766c695515b67225cdacd52dee155605a
Greg Tonoski <111286121+GregTonoski@users.noreply.github.com>
2023-10-20 11:08:47 +0200

    Update version to 5.0.0 version. Release date: 20231020T090800Z in help text

--- BIP39-XOR.sh
+++ BIP39-XOR.sh
@@ -22,7 +22,7 @@ helptext_show () {
   printf "Use input keys to decrypt and output a key. Or, equivalently, encrypt an input key with another one and output two complementary encryption keys in BIP39 format.\n\n"
   printf "$ bash BIP39-XOR.sh --auto-input\n"
   printf "Generate input BIP-39 twelve codewords randomly and output two complementary encryption keys encoded in BIP39 codewords.\n\n"
-  echo "This is the 4.9.9 version. Release date: 20231019T191300Z. Author: Greg Tonoski <greg.tonoski@gmail.com>."
+  echo "This is the 5.0.0 version. Release date: 20231020T090800Z. Author: Greg Tonoski <greg.tonoski@gmail.com>."
 }
 
 INPUT_CODEWORDS_ARRAY1=""
